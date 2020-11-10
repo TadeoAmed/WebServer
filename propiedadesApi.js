59d9af21e8635f05c95c73e632d3a61d0134ccf8
unknown <amedtadeo@gmail.com>
2020-11-10 20:51:24 -0300

    cree post, get(de todas las propiedades), get por id, update por id(no funciona, el problema es el req.body) y el delete por id

--- propiedadesApi.js
+++ propiedadesApi.js
@@ -1,8 +1,12 @@
 
+const express = require ('express');
+const bodyParser = require('body-parser');
+const propiedad = require('./models/propiedad');
+const app = express();
 module.exports = function(app){
 const Propiedad = require('./models/propiedad')
     nuevaPropiedad = function(req,res){
-        let propiedad = new Propiedad()
+        const propiedad = new Propiedad()
         propiedad.direccion = req.body.direccion
         propiedad.tipo = req.body.tipo
         propiedad.piso = req.body.piso
@@ -22,4 +26,55 @@ const Propiedad = require('./models/propiedad')
             res.status(200).send({propiedad: propRegistered}) })
     }
     app.post('/propiedadesApi/propiedades', nuevaPropiedad);
+
+    app.get('/propiedadesApi/propiedades', (req, res) =>{
+        Propiedad.find()
+        .then(doc =>{
+            res.json(doc);
+        })
+        .catch(err =>{
+            console.log('error al consultar');
+        })
+    });
+    
+    app.get('/propiedadesApi/propiedades/:id', (req, res) => {
+        const id = req.params.id
+        Propiedad.findById(id, (err,propencontrada) => {
+           if(err){
+               res.status(500).send(`Error al actualizar propiedad: ${err}`)
+               return
+           }
+           res.status(200).send({propiedad: propencontrada})
+
+           })
+    });
+    app.put('propiedadesApi/propiedades/:id', (req, res) => {
+         const id = req.params.id
+         let update = req.body
+
+         Propiedad.findByIdAndUpdate({_id:id}, update, { useFindAndModify: false }, (err, propiedadupdt) => {
+            if(err){
+                res.status(500).send(`Error al actualizar propiedad: ${err}`)
+                return
+            }   
+            res.status(200).send({ propiedad: propiedadupdt })
+            
+         })
+
+     });
+
+     app.delete('/propiedadesApi/propiedades/:id', (req, res) => {
+        const id = req.params.id
+        
+        propiedad.findByIdAndDelete(id, (err,propencontrada) => {
+           if(err){
+               res.status(500).send(`Error al actualizar propiedad: ${err}`)
+               return
+           }
+           res.status(200).send({propiedad: propencontrada})
+
+           })
+    });
 }
+
+
