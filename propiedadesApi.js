a6f45a05c6900ac588ac15087f314d4ffe313d4d
unknown <amedtadeo@gmail.com>
2020-11-19 20:53:26 -0300

    saque la funcion nuevapropiedad

--- propiedadesApi.js
+++ propiedadesApi.js
@@ -5,27 +5,8 @@ const propiedad = require('./models/propiedad');
 const app = express();
 module.exports = function(app){
 const Propiedad = require('./models/propiedad')
-    nuevaPropiedad = function(req,res){
-        const propiedad = new Propiedad()
-        propiedad.direccion = req.body.direccion
-        propiedad.tipo = req.body.tipo
-        propiedad.piso = req.body.piso
-        propiedad.mst_cuadrados = req.body.mst_cuadrados
-        propiedad.ambiente = req.body.ambiente
-        propiedad.banos = req.body.banos
-        propiedad.barrio = req.body.barrio
-        propiedad.garage = req.body.garage
-        propiedad.balcon = req.body.balcon
-        propiedad.terraza = req.body.terraza
-        propiedad.pileta = req.body.pileta
-        propiedad.save(function(err, propRegistered){
-            if(err){
-                res.status(500).send(`Error al crear propiedad: ${err}`)
-                return
-            }
-            res.status(200).send({propiedad: propRegistered}) })
-    }
-    app.post('/propiedadesApi/propiedades', nuevaPropiedad);
+
+    // app.post('/propiedadesApi/propiedades', nuevaPropiedad);
 
     app.get('/propiedadesApi/propiedades', (req, res) =>{
         Propiedad.find()
@@ -39,15 +20,32 @@ const Propiedad = require('./models/propiedad')
     
     app.get('/propiedadesApi/propiedades/:id', (req, res) => {
         const id = req.params.id
-        Propiedad.findById(id, (err,propencontrada) => {
+        Propiedad.findById(id, (err,id) => {
            if(err){
-               res.status(500).send(`Error al actualizar propiedad: ${err}`)
+               res.status(500).send(`Error al obtener propiedad: ${err}`)
                return
            }
-           res.status(200).send({propiedad: propencontrada})
+           res.status(200).send({propiedad: id})
 
            })
     });
+
+// // get con query string
+
+// app.get('/propiedadesApi/propiedades/', (req,res) => {
+//     const propbusc = req.query
+//      console.log(req.query);
+//     Propiedad.find(propbusc, function(err,doc){
+//         if(err){
+//             res.status(500).send(`Error al actualizar propiedad: ${err}`)
+//             return
+//         }
+//         res.status(200).send({propiedad: propbusc});
+//     });
+ 
+//  })
+
+
     app.put('propiedadesApi/propiedades/:id', (req, res) => {
          const id = req.params.id
          let update = req.body
